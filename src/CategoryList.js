48bdef57cb504dc9fa417b41be4016d7970a8e5b
emreesnn <emreesnn17@gmail.com>
2022-08-02 15:55:52 +0300

    State Kullanımı

--- src/CategoryList.js
+++ src/CategoryList.js
@@ -1,22 +1,25 @@
 import React, { Component } from "react";
-import {ListGroup,ListGroupItem} from "reactstrap"
+import { ListGroup, ListGroupItem } from "reactstrap";
 export default class CategoryList extends Component {
-  /* Artık Yeni sürümlerle zorunlu değil
-  constructor(props){
-      super(props);
-      state:{}
-  }
-  */
+  state = {
+    categories: [
+      { categoryId: 1, categoryName: "Yiyecek" },
+      { categoryId: 2, categoryName: "İçecek" },
+    ],
+  };
   render() {
     return (
       <div>
         <h3>{this.props.info.title}</h3>
         <ListGroup>
-          <ListGroupItem>Cras justo odio</ListGroupItem>
-          <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-          <ListGroupItem>Morbi leo risus</ListGroupItem>
-          <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
-          <ListGroupItem>Vestibulum at eros</ListGroupItem>
+          {
+            //map = foreach benzeri bir döngü sağlar
+            this.state.categories.map((category) => (
+              <ListGroupItem key={category.categoryId}>
+                {category.categoryName}
+              </ListGroupItem>
+            ))
+          }
         </ListGroup>
       </div>
     );
