59e4162fb5c096d3bc2a0c5746b4453e068fa016
emreesnn <emreesnn17@gmail.com>
2022-08-10 00:34:46 +0300

    Componentlere Redux Entegre Edildi

--- northwind-redux/src/components/categories/CategoryList.js
+++ northwind-redux/src/components/categories/CategoryList.js
@@ -1,9 +1,19 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-export default class CategoryList extends Component {
+class CategoryList extends Component {
   render() {
     return (
-      <div>CategoryList</div>
-    )
+      <div>
+        <h1>Category List</h1>
+        <h3>Seçili Kategori : {this.props.currentCategory.categoryName}</h3>
+      </div>
+    );
   }
 }
+
+function mapStateToProps(state) {
+  return { currentCategory: state.changeCategoryReducer };
+}
+
+export default connect(mapStateToProps)(CategoryList);
